2fda13e21492e7db73705bd28cd339db81be7522
Jia Jun <terhuncode@gmail.com>
2025-04-16 19:41:13 +0800

    comment img

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
       <h1>Vite + React</h1>
       <h2>My name is Zhi Qing hahahaha I am Zhi Qing Hey Hey</h2>
