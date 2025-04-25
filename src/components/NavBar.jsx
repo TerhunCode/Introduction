854316dc8ee2f942252b08a3fe69c047ed295bb6
Jia Jun <terhuncode@gmail.com>
2025-04-25 11:59:44 +0800

    navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import "../css/NavBar.css";
 
 function NavBar() {
     return(
         <nav className="navbar">
-            <div>
+            <div className="navbar-links">
                 <Link to="/">Home</Link>
                 <Link to="/project">Project</Link>
+                <Link to="/project">Experiences</Link>
             </div>
         </nav>
     )
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,7 +1,7 @@
 function Project() {
   return (
-    <div>
-      <h1>Project Page</h1>
+    <div style={{ width: "100%" }}>
+      <p>Project Page</p>
       <p>This is the project page content.</p>
     </div>
   );
